8447c70e6717656b4ff24ac74fe5350f67c9df0e
Po Chen <chenpaul914@gmail.com>
2016-10-04 11:53:57 +1100

    publish

--- mix.exs
+++ mix.exs
@@ -3,11 +3,21 @@ defmodule SideEffect.Mixfile do
 
   def project do
     [app: :side_effect,
+     description: description(),
      version: "0.1.0",
      elixir: "~> 1.3",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
-     deps: deps()]
+     deps: deps(),
+     package: package()]
+  end
+
+  defp package do
+    [name: :side_effect,
+     files: ["lib", "mix.exs", "README*", "LICENSE*"],
+     maintainers: ["Po Chen"],
+     licenses: ["MIT"],
+     links: %{"GitHub" => "https://github.com/princemaple/elixir-side-effect"}]
   end
 
   # Configuration for the OTP application
@@ -15,6 +25,10 @@ defmodule SideEffect.Mixfile do
   # Type "mix help compile.app" for more information
   def application do
     [applications: [:logger]]
+  end
+
+  defp description do
+    "simple utils for making side effect calls in pipelines"
   end
 
   # Dependencies can be Hex packages:
@@ -27,6 +41,6 @@ defmodule SideEffect.Mixfile do
   #
   # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:ex_doc, ">= 0.0.0", only: :dev}]
   end
 end
