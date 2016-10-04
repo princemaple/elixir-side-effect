1fc1a07eff828eb2b0b2a6d3fded2fe1e2f347c6
Po Chen <chenpaul914@gmail.com>
2016-10-04 11:26:21 +1100

    all in

--- lib/side_effect.ex
+++ lib/side_effect.ex
@@ -1,2 +1,38 @@
 defmodule SideEffect do
+  @doc """
+  iex> 1
+  ...> |> SideEffect.side_call(2)
+  1
+  """
+  def side_call(piped_value, _) do
+    piped_value
+  end
+
+  @doc """
+  iex> -1
+  ...> |> SideEffect.side_apply(&abs/1)
+  -1
+  """
+  def side_apply(piped_value, func) when is_function(func) do
+    func.(piped_value)
+    piped_value
+  end
+
+  @doc """
+  iex> 1
+  ...> |> SideEffect.side_apply(IO, :inspect)
+  1
+
+  iex> 1
+  ...> |> SideEffect.side_apply(IO, :inspect, [[base: :hex]])
+  ...> |> List.wrap
+  [1]
+  """
+  def side_apply(piped_value, module, func, other_args \\ [], position \\ 0)
+      when is_atom(module) and is_atom(func) and is_list(other_args) do
+    Kernel.apply(module, func,
+      Enum.take(other_args, position) ++
+      [piped_value | Enum.drop(other_args, position)])
+    piped_value
+  end
 end
--- test/side_effect_test.exs
+++ test/side_effect_test.exs
@@ -2,7 +2,34 @@ defmodule SideEffectTest do
   use ExUnit.Case
   doctest SideEffect
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  defp echo_server do
+    receive do
+      {from, any} ->
+        send from, any
+    end
+    echo_server
+  end
+
+  setup_all _context do
+    [echo: spawn(&echo_server/0)]
+  end
+
+  test "it makes a call as a side effect", %{echo: echo} do
+    assert SideEffect.side_call(1, send(echo, {self(), :sent})) == 1
+    assert_receive :sent
+  end
+
+  test "it applies the function as a side effect", %{echo: echo} do
+    assert SideEffect.side_apply(1, &send(echo, {self(), &1})) == 1
+    assert_receive 1
+  end
+
+  def test(echo, a, b, c) do
+    send echo, {self(), [a, b, c]}
+  end
+
+  test "it applies the module function as a side effect", %{echo: echo} do
+    assert SideEffect.side_apply(1, SideEffectTest, :test, [echo, 2, 3], 1) == 1
+    assert_receive [1, 2, 3]
   end
 end
